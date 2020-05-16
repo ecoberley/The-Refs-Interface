5baa8f8211c66ca92637fdc1cd1afeb12de436cf
Evan Coberley <ecoberley@ou.edu>
2020-05-16 17:25:32 -0500

    Added search_cybernetics feature

--- src/commands.py
+++ src/commands.py
@@ -1,5 +1,6 @@
 from src.quick_functions import *
 from src.weapons import *
+from src.cybernetics import *
 
 cmd_dict = {
     # Finds the hit difficulty of a given shot from
@@ -7,6 +8,7 @@ cmd_dict = {
     "to_hit": to_hit,
     "search_weapons": search_weapons,
     "weapon_desc_format": weapon_desc_format,
+    "search_cybernetics": search_cybernetics,
     "roll": roll
 
 }
---
+++ src/cybernetics.py
@@ -0,0 +1,30 @@
+import xlrd
+from src.cybernetic import *
+
+filename = "cybernetics_list.xlsx"
+cybernetics_spreadsheet = xlrd.open_workbook(filename).sheet_by_index(0)
+
+cybernetics = []
+for i in range(1, cybernetics_spreadsheet.nrows):
+    temp_cybernetic = Cybernetic(cybernetics_spreadsheet.row_values(i))
+    cybernetics.append(temp_cybernetic)
+print("Imported cybernetics_list...")
+
+
+def search_cybernetics(args):
+    results = cybernetic_desc_format(0)
+    query = args[1].lower()
+    for idx in range(0, len(cybernetics)):
+        if cybernetics[idx].to_string().lower().__contains__(query):
+            if idx != len(cybernetics) - 1:
+                results += cybernetics[idx].to_string() + "\n"
+            else:
+                results += cybernetics[idx].to_string()
+    if results == cybernetic_desc_format(0):
+        return "No results found"
+    else:
+        return results
+
+
+def cybernetic_desc_format(args):
+    return "[Name • Surgery Type • Cost • Humanity Loss •  Availability • Description]\n"
